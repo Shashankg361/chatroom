fc718ea98f625767241be837059113ad2b072842
shashankg361 <shashankg361@gmail.com>
2022-12-20 01:34:52 +0530

    added messaging function and displaying it

--- Script/app.js
+++ Script/app.js
@@ -1,9 +1,18 @@
 //reference to the DOM
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
 
 //class instences
 const chatUI = new ChatUI(chatList);
 const chatroom = new ChatRoom('Yoshi','general');
+
+// adding Eventlisterner
+newChatForm.addEventListener('submit',e=>{
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message);
+})
+
 
 
 // gey chat and render
